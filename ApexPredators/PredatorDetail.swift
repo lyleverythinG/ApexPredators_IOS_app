fcbd9958c257e3e75d8e34687362829f7b8188f9
Lyle Dane Carcedo <lylecarcedo@gmail.com>
2024-11-22 14:41:39 +0800

    Add predator detail UI and link

--- ApexPredators/PredatorDetail.swift
+++ ApexPredators/PredatorDetail.swift
@@ -27,16 +27,45 @@ struct PredatorDetail: View {
                     .shadow(color: .black,radius: 7)
                     .offset(y:20)
             }
-            
-            // Dino Name
-            
-            // Current Location
-            
-            // Appears in
-            
-            // Movie moments
-            
-            // Link
+            VStack(alignment: .leading) {
+                // Dino Name
+                Text(predator.name)
+                    .font(.largeTitle)
+                // TODO: Handle current location later.
+                // Current Location
+                
+                // Appears in
+                Text("Appears In:")
+                    .font(.title3)
+                
+                ForEach(predator.movies, id: \.self) { movie in
+                    Text("• " + movie)
+                        .font(.subheadline)
+    
+                }
+                // Movie moments Text
+                Text("Movie Moments")
+                    .font(.title)
+                    .padding(.top, 15)
+    
+                // Title and description of movies
+                ForEach(predator.movieScenes) { scene in
+                    Text(scene.movie)
+                        .font(.title2)
+                        .padding(.vertical, 1)
+                    
+                    Text(scene.sceneDescription)
+                        .padding(.bottom, 15)
+                }
+                
+                // Link
+                // TODO: Avoid this force unwrapping behavior. Follow the course then improve code quality once finish. Also add each link to each movie.
+                Text("Read More:")
+                    .font(.caption)
+                Link (predator.link, destination: URL(string:predator.link)!)
+            }
+            .padding()
+            .frame(width:geo.size.width,alignment: .leading)
         }
         .ignoresSafeArea()
     }
